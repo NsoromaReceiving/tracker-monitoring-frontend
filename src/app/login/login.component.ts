d634cf57127bd84d572e5fff987fd7ad09067606
Banabas <banabasaveabadu@gmail.com>
2019-12-08 14:22:50 +0000

    implemented login and token storing functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { APIcallsService } from '../apicalls.service';
+import { CookieService } from 'ngx-cookie-service';
+
+interface User {
+  user;
+  password;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm;
+  invalidLogin = false;
+
+  constructor(private router: Router, private apiService: APIcallsService, private cookies: CookieService) { }
 
   ngOnInit() {
+
+    this.loginForm = new FormGroup({
+      password: new FormControl(null, [Validators.required]),
+      user: new FormControl(null, [Validators.required]),
+    });
+  }
+
+
+  onSubmit(user) {
+    this.invalidLogin = false;
+    console.log(user);
+    this.apiService.login(user).subscribe((result: any) => {
+      this.invalidLogin = false;
+      this.cookies.set('token', result.token);
+      console.log(result);
+      this.router.navigate(['']);
+      },
+      (error) => {
+        if (error.status === 401) {
+          this.invalidLogin = true;
+        }
+      });
   }
 
 }
