027750b4fe8d3e986e057fe3634e408e5958db50
Banabas <banabasaveabadu@gmail.com>
2019-12-08 16:31:33 +0000

    implemented Route Guard to check if user is authenticated to the main-app

---
+++ src/app/guards/auth-guard.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { APIcallsService } from '../apicalls.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private apiService: APIcallsService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import ('@angular/router').UrlTree |
+  Observable<boolean | import ('@angular/router').UrlTree> | Promise<boolean | import ('@angular/router').UrlTree> {
+    console.log('can activate ?');
+    return this.apiService.validateToken().pipe(
+      map(e => {
+        if (e) {
+          return true;
+        } else {
+          this.router.navigate(['login']);
+          return false;
+        }
+      }),
+      catchError((err) => {
+        if (err.status === 200) {
+          return of(true);
+        }
+        this.router.navigate(['login']);
+        return of(false);
+      })
+    );
+  }
+
+}
